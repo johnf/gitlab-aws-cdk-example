f893a49dddc575067447542fe41b766246e78c5c
John Ferlito <johnf@inodes.org>
2024-09-17 18:15:08 +1000

    Add gitlab setup

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
-# Welcome to your CDK TypeScript project
+# Setup AWS to support OIDC to gitlab
 
-This is a blank project for CDK development with TypeScript.
+Sets up the IAM provider and roles to allow GitLab to connect without secrets
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Setup a new account
+
+``` bash
+export AWS_PROFILE=xxx-dev
+ACCOUNT_NUMBER=$(aws sts get-caller-identity | jq -r .Account)
+
+# First time
+npx cdk --profile "$AWS_PROFILE" bootstrap aws://${ACCOUNT_NUMBER}/ap-southeast-2
+
+# Every time
+npx cdk diff
+npx cdk deploy
+
+
+## GitLab Setup
+
+Add something like this to the github CI config
+NOTE: You should define ROLE_ARN in the project settings as the output from the cloudformation stack
+
+```yaml
+assume role:
+  id_tokens:
+    GITLAB_OIDC_TOKEN:
+      aud: sts.amazonaws.com
+  before_script:
+    - >
+      mkdir -p ~/.aws
+      echo "${MY_OIDC_TOKEN}" > /tmp/web_identity_token
+      echo -e "[profile oidc]\nrole_arn=${ROLE_ARN}\nweb_identity_token_file=/tmp/web_identity_token" > ~/.aws/config
+  # NOTE: If any tooling doesn't support AWS config files then you can do the below to get env variables
+  # before_script:
+  #   - >
+  #     export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s"
+  #     $(aws sts assume-role-with-web-identity
+  #     --role-arn ${ROLE_ARN}
+  #     --role-session-name "GitLabRunner-${CI_PROJECT_ID}-${CI_PIPELINE_ID}"
+  #     --web-identity-token ${GITLAB_OIDC_TOKEN}
+  #     --duration-seconds 3600
+  #     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]'
+  #     --output text))
+  script:
+    - aws sts get-caller-identity
+```
 
 ## Useful commands
 
--- lib/gitlab-oidc-cdk-stack.ts
+++ lib/gitlab-oidc-cdk-stack.ts
@@ -1,16 +1,84 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import type { Construct } from 'constructs';
+
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class GitlabOidcCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const provider = new iam.OpenIdConnectProvider(this, 'GitLabOidcProvider', {
+      url: 'https://gitlab.com',
+      clientIds: ['sts.amazonaws.com'],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GitlabOidcCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const org = 'orgname';
+    const repo = 'app';
+    const branch = 'dev';
+    const sub = `${org}/${repo}:ref_type:branch:ref:${branch}`;
+    // Or you can allow all branches
+    // const sub = `${org}/${repo}:*`;
+
+    const role = new iam.Role(this, 'GitLabOidcRole', {
+      assumedBy: new iam.WebIdentityPrincipal(provider.openIdConnectProviderArn, {
+        StringEquals: {
+          'gitlab.com:aud': 'sts.amazonaws.com',
+        },
+        StringLike: {
+          'gitlab.com:sub': sub,
+        },
+      }),
+      roleName: 'GitlabOidcRole',
+    });
+
+    const policy = new iam.ManagedPolicy(this, 'GitLabPipelineDeploymentPolicy', {
+      description: 'Git Lab Pipeline Deployment Policy',
+      managedPolicyName: 'GitLabPipelineDeploymentPolicy',
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          // TODO: Add any actions you need to work in Gitlab here
+          actions: [
+            'cloudformation:DescribeChangeSet',
+            'cloudformation:DescribeStackEvents',
+            'cloudformation:DescribeStackResource',
+            'cloudformation:DescribeStackResources',
+            'cloudformation:DescribeStackSet',
+            'cloudformation:DescribeStackSetOperation',
+            'cloudformation:DescribeStacks',
+            'cloudformation:GetTemplate',
+            'cloudformation:GetTemplateSummary',
+            'cloudformation:ListStackResources',
+            'cloudfront:CreateInvalidation',
+            'iam:GetPolicy',
+            'iam:GetRole',
+            'iam:GetRolePolicy',
+            'iam:GetUser',
+            'iam:ListAttachedRolePolicies',
+            'iam:ListPolicyVersions',
+            'iam:PassRole',
+            'iam:PutRolePermissionsBoundary',
+            'iam:PutRolePolicy',
+            'logs:DescribeLogStreams',
+            'logs:GetLogEvents',
+            's3:GetBucketLocation',
+            's3:GetObject',
+            's3:HeadBucket',
+            's3:ListAllMyBuckets',
+            's3:ListBucket',
+            's3:ListBucketVersions',
+            's3:PutObject',
+          ],
+          resources: ['*'],
+        }),
+      ],
+    });
+    role.addManagedPolicy(policy);
+
+    // Output the Role ARN for reference
+    new cdk.CfnOutput(this, 'GitLabRoleArn', {
+      value: role.roleArn,
+      description: 'The ARN of the IAM role for GitLab CI/CD',
+    });
   }
 }
